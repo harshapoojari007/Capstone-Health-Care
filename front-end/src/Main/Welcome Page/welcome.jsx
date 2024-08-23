d54ffe889223c942e17213051c0350718bd316dc
user1 <namanbhat002@gmail.com>
2024-08-23 11:04:03 +0200

    second stage changes

--- front-end/src/Main/Welcome Page/welcome.jsx
+++ front-end/src/Main/Welcome Page/welcome.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Button from "../../Components/UIElements/Button";
 import "./welcome.css"
-import Carousal from "../../Components/UIElements/Carousal";
 const Welcome=()=>{
   const myStyles={
     width:"10rem",
@@ -10,21 +9,8 @@ const Welcome=()=>{
   }
 
   return (<div className="welcome_main">
-    <h1>hello welcome</h1>
-    <ul>
-      <li>
-        <NavLink to={"login"}>login</NavLink>
-      </li>
-      <li>
-        <NavLink to={"home"}>Home</NavLink>
-      </li>
-      <li>
-        <Button value="submit" myStyles={myStyles} />
-      </li>
-      <a href="/dashboard">go</a>
-    </ul>
     <div className="carousal">
-      <Carousal/>
+      
     </div>
 
   </div>)
